9e420e4bc0400207cab5b678860bef5f8762e488
Tomasz Kontusz <tomasz.kontusz@gmail.com>
2018-03-22 21:11:13 +0100

    Przejście na Bootstrapa

--- src/Main.elm
+++ src/Main.elm
@@ -1,29 +1,69 @@
 module Main exposing (..)
 
 import Html exposing (Html, div, program, text)
+import Html.Attributes exposing (href)
+import Bootstrap.CDN as CDN
+import Bootstrap.Card as Card
+import Bootstrap.Card.Block as Block
+import Bootstrap.Grid as Grid
+import Bootstrap.Navbar as Navbar
 
 
 type alias Model =
-    {}
+    { navbar : Navbar.State
+    }
 
 
 type Msg
     = NoOp
+    | NavbarMsg Navbar.State
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    let
+        ( navbarState, navbarMsg ) =
+            Navbar.initialState NavbarMsg
+    in
+        ( { navbar = navbarState
+          }
+        , Cmd.batch
+            [ navbarMsg
+            ]
+        )
 
 
 view : Model -> Html Msg
 view model =
-    text "Hello!"
+    Html.div []
+        [ CDN.stylesheet
+        , Navbar.config NavbarMsg
+            |> Navbar.withAnimation
+            |> Navbar.collapseMedium
+            -- Collapse menu at the medium breakpoint
+            |> Navbar.primary
+            -- Customize coloring
+            |> Navbar.brand [ href "#" ] [ text "LunchPlan" ]
+            |> Navbar.view model.navbar
+        , Grid.container []
+            [ Card.config [ Card.attrs [ Html.Attributes.style [ ( "width", "20rem" ) ] ] ]
+                |> Card.block []
+                    [ Block.titleH4 [] [ text "Siema!" ]
+                    , Block.text [] [ text "Przykładowa karta z kawałkiem tekstu." ]
+                    ]
+                |> Card.view
+            ]
+        ]
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    model ! []
+    case msg of
+        NoOp ->
+            model ! []
+
+        NavbarMsg state ->
+            { model | navbar = state } ! []
 
 
 subscriptions : Model -> Sub Msg
