f0c036c29fd5345e18621ac712d8f9c672d6c0c3
Tomasz Kontusz <tomasz.kontusz@gmail.com>
2018-03-24 16:53:46 +0100

    Pierwsza makieta

--- src/Main.elm
+++ src/Main.elm
@@ -2,15 +2,51 @@ module Main exposing (..)
 
 import Html exposing (Html, div, program, text)
 import Html.Attributes exposing (href)
+import Dict exposing (Dict)
 import Bootstrap.CDN as CDN
 import Bootstrap.Card as Card
 import Bootstrap.Card.Block as Block
+import Bootstrap.ListGroup as ListGroup
 import Bootstrap.Grid as Grid
+import Bootstrap.Grid.Col as Col
 import Bootstrap.Navbar as Navbar
+import Bootstrap.Utilities.Spacing as Spacing
 
 
 type alias Model =
     { navbar : Navbar.State
+    , orders : List Order
+    }
+
+
+type OrderStatus
+    = Proposed
+    | Championed
+    | Ordered
+
+
+type alias Order =
+    { place : Place
+    , positions : List Position
+    , status : OrderStatus
+    }
+
+
+type alias Position =
+    { participant : Participant
+    , description : String
+    , champion : Bool
+    }
+
+
+type alias Participant =
+    { name : String }
+
+
+type alias Place =
+    { name : String
+    , link : String
+    , description : String
     }
 
 
@@ -26,6 +62,75 @@ init =
             Navbar.initialState NavbarMsg
     in
         ( { navbar = navbarState
+          , orders =
+                [ { place =
+                        { name = "Chicago's Pizza"
+                        , description = "Na przeciwko"
+                        , link = "TODO"
+                        }
+                  , positions =
+                        [ { participant = { name = "Piesio Grzesio" }
+                          , champion = False
+                          , description = "Texas"
+                          }
+                        , { participant = { name = "Kot Psot" }
+                          , champion = True
+                          , description = "Cztery sery"
+                          }
+                        ]
+                  , status = Championed
+                  }
+                , { place =
+                        { name = "TeleSajgon"
+                        , description = "Chińczyk z pudłami"
+                        , link = "http://www.telesajgon.pl/ken.html"
+                        }
+                  , positions =
+                        [ { participant = { name = "Piesio Grzesio" }
+                          , champion = False
+                          , description = "Duża zupa MIEN z makaronem sojowym i wołowiną"
+                          }
+                        , { participant = { name = "Kot Psot" }
+                          , champion = False
+                          , description = "Banan w cieście"
+                          }
+                        , { participant = { name = "Piesio Grzesio" }
+                          , champion = False
+                          , description = "Duża zupa MIEN z makaronem sojowym i wołowiną"
+                          }
+                        , { participant = { name = "Kot Psot" }
+                          , champion = False
+                          , description = "Banan w cieście"
+                          }
+                        , { participant = { name = "Piesio Grzesio" }
+                          , champion = False
+                          , description = "Duża zupa MIEN z makaronem sojowym i wołowiną"
+                          }
+                        , { participant = { name = "Kot Psot" }
+                          , champion = False
+                          , description = "Banan w cieście"
+                          }
+                        ]
+                  , status = Championed
+                  }
+                , { place =
+                        { name = "Dominos"
+                        , description = "nie \"Dominium\""
+                        , link = "TODO"
+                        }
+                  , positions =
+                        [ { participant = { name = "Piesio Grzesio" }
+                          , champion = True
+                          , description = "Duża zupa MIEN z makaronem sojowym i wołowiną"
+                          }
+                        , { participant = { name = "Kot Psot" }
+                          , champion = False
+                          , description = "Banan w cieście"
+                          }
+                        ]
+                  , status = Championed
+                  }
+                ]
           }
         , Cmd.batch
             [ navbarMsg
@@ -45,15 +150,59 @@ view model =
             -- Customize coloring
             |> Navbar.brand [ href "#" ] [ text "LunchPlan" ]
             |> Navbar.view model.navbar
-        , Grid.container []
-            [ Card.config [ Card.attrs [ Html.Attributes.style [ ( "width", "20rem" ) ] ] ]
-                |> Card.block []
-                    [ Block.titleH4 [] [ text "Siema!" ]
-                    , Block.text [] [ text "Przykładowa karta z kawałkiem tekstu." ]
-                    ]
-                |> Card.view
-            ]
+        , model.orders
+            |> List.map orderCard
+            |> flip List.append [ newOrderCard ]
+            |> cardsList
         ]
+
+
+cardsList : List (Card.Config Msg) -> Html Msg
+cardsList cards =
+    Grid.containerFluid []
+        [ cards
+            |> List.map
+                (\c ->
+                    Grid.col [ Col.md4 ] [ Card.view c ]
+                )
+            |> Grid.row []
+        ]
+
+
+newOrderCard : Card.Config Msg
+newOrderCard =
+    Card.config [ Card.attrs [ Spacing.mb3 ] ]
+        |> Card.headerH4 [] [ text "Zaproponuj kolejne miejsce:" ]
+        |> Card.block []
+            [ Block.text [] [ text "TODO: formularz nowego miejsca :-)" ]
+            ]
+
+
+orderCard : Order -> Card.Config Msg
+orderCard order =
+    Card.config [ Card.attrs [ Spacing.mb3 ] ]
+        |> Card.headerH4 []
+            [ text order.place.name ]
+        |> Card.block []
+            [ Block.text []
+                [ Html.a [ href order.place.link ] [ text order.place.link ]
+                ]
+            , Block.text []
+                [ text order.place.description
+                ]
+            ]
+        |> Card.listGroup
+            (order.positions
+                |> List.map orderPosition
+                |> List.map (ListGroup.li [])
+            )
+
+
+orderPosition : Position -> List (Html Msg)
+orderPosition { participant, description } =
+    [ div [] [ text participant.name ]
+    , div [ Html.Attributes.align "right" ] [ text description ]
+    ]
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
